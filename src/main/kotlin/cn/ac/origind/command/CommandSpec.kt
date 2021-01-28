a703d31772c1dc1a87ede0cb07f3dbf5801aafa9
Lasm Gratel <lasm_gratel@hotmail.com>
2021-01-28 17:07:21 +0800

    sb sudo

--- src/main/kotlin/cn/ac/origind/command/CommandSpec.kt
+++ src/main/kotlin/cn/ac/origind/command/CommandSpec.kt
@@ -1,6 +1,36 @@
 package cn.ac.origind.command
 
 open class CommandSpec(val name: String) {
+    val subcommands = mutableListOf<CommandSpec>()
+    val subcommandMap = mutableMapOf<String, CommandSpec>()
+    var permission: String = "destinybot.$name"
+
+    open fun parse(parser: CommandParser, executor: CommandExecutor, context: CommandContext) {
+        val sub = parser.take(false)
+        if (subcommandMap.containsKey(sub)) {
+            parser.take()
+            subcommandMap[sub]?.parse(parser, executor, context)
+        }
+    }
+
+    open fun getHelp() = buildString {
+    }
+
+    fun registerSubcommand(command: CommandSpec) {
+        subcommands += command
+        subcommandMap[command.name] = command
+    }
+
+    fun subcommand(name: String, init: CommandSpec.() -> Unit): CommandSpec {
+        val spec = CommandSpec(name)
+        spec.init()
+        registerSubcommand(spec)
+        return spec
+    }
+
+    fun argument(name: String, type: ArgumentType<*>) {
+        
+    }
 }
 
 fun command(name: String, init: CommandSpec.() -> Unit): CommandSpec {
