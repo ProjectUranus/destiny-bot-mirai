cc09d7f890e5737c4f161868d07ee333efc4fb6e
Lasm Gratel <lasm_gratel@hotmail.com>
2021-06-24 18:57:50 +0800

    update

--- src/main/kotlin/cn/ac/origind/minecraft/MinecraftVersionCommands.kt
+++ src/main/kotlin/cn/ac/origind/minecraft/MinecraftVersionCommands.kt
@@ -19,7 +19,21 @@ lateinit var versionMap: Map<String, Version>
 
 suspend fun initMinecraftVersion() {
     versionManifest = withContext(Dispatchers.IO) { mapper.readValue(File("version_manifest.json"), MinecraftVersionManifest::class.java) }
-    versionMap = versionManifest.versions?.map { "/" + it.id!!.replace(".", "") to it }?.toMap() ?: emptyMap()
+    versionMap = versionManifest.versions?.associate { "/" + it.id!!.replace(".", "") to it } ?: emptyMap()
+}
+
+private fun buildMinecraftVersionMessage(version: String, builder: StringBuilder) {
+    val version = versionMap["/" + version.replace(".", "").replace("/", "")]
+    val now = LocalDateTime.now()
+    val duration = Duration.between(Instant.parse(version?.releaseTime), Instant.now())
+    val period = Period.between(ZonedDateTime.parse(version?.releaseTime).toLocalDate(), now.toLocalDate())
+
+    builder.apply {
+        append("Minecraft ${version?.id} 已经发布 ")
+        if (period.years > 0) append("${period.years} 年 ")
+        if (period.months > 0) append("${period.months} 月 ")
+        append("${period.days} 天 ${duration.toHoursPart()} 小时 ${duration.toMinutesPart()} 分钟 ${duration.toSecondsPart()} 秒 ${duration.toMillisPart()} 毫秒了。")
+    }
 }
 
 fun MessageEventSubscribersBuilder.minecraftCommands() {
@@ -29,7 +43,7 @@ fun MessageEventSubscribersBuilder.minecraftCommands() {
     startsWith("/ping ") {
         val address = it
         if (config[MinecraftSpec.servers].containsKey(it)) {
-            val spec = config[MinecraftSpec.servers][it.removePrefix("/ping ")]
+            val spec = config[MinecraftSpec.servers][it]
             MinecraftClientLogin.statusAsync(subject, spec!!.host!!, spec.port)
             return@startsWith
         }
@@ -48,52 +62,23 @@ fun MessageEventSubscribersBuilder.minecraftCommands() {
         }
     }
     case("/release") {
-        val version = versionMap["/" + versionManifest.latest?.release?.replace(".", "")]
-        val now = LocalDateTime.now()
-        val duration = Duration.between(Instant.parse(version?.releaseTime), Instant.now())
-        val period = Period.between(ZonedDateTime.parse(version?.releaseTime).toLocalDate(), now.toLocalDate())
-
-        reply(buildString {
-            append("最新版本 Minecraft ${version?.id} 已经发布 ")
-            if (period.years > 0) append("${period.years} 年 ")
-            if (period.months > 0) append("${period.months} 月 ")
-            append("${period.days} 天 ${duration.toHoursPart()} 小时 ${duration.toMinutesPart()} 分钟 ${duration.toSecondsPart()} 秒 ${duration.toMillisPart()} 毫秒了, ")
-            append("你怎么还不去玩 Minecraft ${version?.id}?")
-        })
+        val builder = StringBuilder("最新版本")
+        buildMinecraftVersionMessage(versionManifest.latest?.release!!, builder)
+        reply(builder.toString())
     }
     case("/latest") {
-        val version = versionMap["/" + versionManifest.latest?.snapshot?.replace(".", "")]
-        val now = LocalDateTime.now()
-        val duration = Duration.between(Instant.parse(version?.releaseTime), Instant.now())
-        val period = Period.between(ZonedDateTime.parse(version?.releaseTime).toLocalDate(), now.toLocalDate())
-
-        reply(buildString {
-            if (versionManifest.latest?.snapshot == versionManifest.latest?.release)
-                append("最新版本 Minecraft ${version?.id} 已经发布 ")
-            else
-                append("最新预览版 Minecraft ${version?.id} 已经发布 ")
-            if (period.years > 0) append("${period.years} 年 ")
-            if (period.months > 0) append("${period.months} 月 ")
-            append("${period.days} 天 ${duration.toHoursPart()} 小时 ${duration.toMinutesPart()} 分钟 ${duration.toSecondsPart()} 秒 ${duration.toMillisPart()} 毫秒了, ")
-            append("你怎么还不去玩 Minecraft ${version?.id}?")
-        })
+        val builder = if (versionManifest.latest?.snapshot == versionManifest.latest?.release) {
+            StringBuilder("最新版本 ")
+        }
+        else {
+            StringBuilder("最新预览版 ")
+        }
+        buildMinecraftVersionMessage(versionManifest.latest?.snapshot!!, builder)
+        reply(builder.toString())
     }
     content { versionMap.containsKey(it) }.reply {
-        val version = versionMap[it]
-        val now = LocalDateTime.now()
-        val duration = Duration.between(Instant.parse(version?.releaseTime), Instant.now())
-        val period = Period.between(ZonedDateTime.parse(version?.releaseTime).toLocalDate(), now.toLocalDate())
-
-        buildString {
-            append("Minecraft ${version?.id} 已经发布 ")
-            if (period.years > 0) append("${period.years} 年 ")
-            if (period.months > 0) append("${period.months} 月 ")
-            append("${period.days} 天 ${duration.toHoursPart()} 小时 ${duration.toMinutesPart()} 分钟 ${duration.toSecondsPart()} 秒 ${duration.toMillisPart()} 毫秒了, ")
-            if (period.years > 1) {
-                append("你为什么还在玩 Minecraft ${version?.id}?")
-            } else {
-                append("你怎么还不去玩 Minecraft ${version?.id}?")
-            }
-        }
+        val builder = StringBuilder()
+        buildMinecraftVersionMessage(it, builder)
+        builder.toString()
     }
 }
--- src/test/kotlin/cn/ac/origind/destinybot/TestBot.kt
+++ src/test/kotlin/cn/ac/origind/destinybot/TestBot.kt
@@ -1,22 +1,12 @@
 package cn.ac.origind.destinybot
 
-import cn.ac.origind.destinybot.command.destinyBrigadierCommands
 import cn.ac.origind.destinybot.image.getImage
-import com.mojang.brigadier.CommandDispatcher
 import kotlinx.coroutines.runBlocking
-import net.mamoe.mirai.contact.MemberPermission
-import net.mamoe.mirai.event.events.GroupMessageEvent
-import net.mamoe.mirai.event.events.MessageEvent
 import org.junit.Test
 
 class TestBot {
     @Test
     fun testWeeklyReport(): Unit = runBlocking {
         getImage("https:${getLatestWeeklyReportURL()}", false)
-    }
-
-    fun testBrigadier(): Unit = runBlocking {
-        val dispatcher = CommandDispatcher<MessageEvent>()
-        destinyBrigadierCommands(dispatcher)
     }
 }
